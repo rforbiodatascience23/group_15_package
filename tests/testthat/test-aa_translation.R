a48223e080aeb39c1a190006e421b189a43d2e0e
KristineRosted <kristine.rosted@gmail.com>
2023-10-24 08:44:33 +0000

    adding a function called aa_translation with a test to it

---
+++ tests/testthat/test-aa_translation.R
@@ -0,0 +1,8 @@
+test_that("aa_translation returns expected aminoacids", {
+  codons <- c("AUG", "CUA", "GUG", "GAG", "ACG")
+  result <- aa_translation(codons)
+  expected <- "MLVET"
+  expect_equal(result, expected)
+})
+
+
